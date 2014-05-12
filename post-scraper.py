6ffd5cf335597e7eadd193481d4b98d6319fe4f3
Mohmed Jasim P <mohd.jas@gmail.com>
2014-05-12 18:10:09 +0300

    Made comments inline to keep it pretty.

--- post-scraper.py
+++ post-scraper.py
@@ -14,7 +14,9 @@ def getListing(after=""):
 	headers = { 
 		'User-Agent': 'Reddit Post Scraper 0.1' 
 	}
-	r = requests.get(url, headers=headers)												#Gets JSON file at url
+	
+	#Gets JSON file at url
+	r = requests.get(url, headers=headers)												
 	if r.status_code == requests.codes.ok:
 		data = r.json()
 		time.sleep(1)
@@ -30,42 +32,53 @@ def savePostsToFile(posts):
 	posts -- Children of the page listing
 	"""
 	
-	global counter																								#Use the global counter variable
+	#Use the global counter variable
+	global counter																								
 	
 	for post in posts:
-		postTitle = post['data']['title']														#Get the title of the post
+		#Get the title of the post
+		postTitle = post['data']['title']														
 		print(postTitle)
 		
-		try:																												#Check the title for the given expected pattern
+		#Check the title for expected pattern
+		try:																												
 			difficulty = re.compile('(easy|medium|intermediate|hard|difficult)', re.I).search(postTitle).group().lower()
 			challenge = re.compile('#[0-9]+').search(postTitle).group()
 			desc = post['data']['selftext']
 			permalink = post['data']['permalink']
-		except:																											#If an exception is raised, it means it is not a challenge post. So skip it.
+		#If an exception is raised, it means it is not a challenge post. So skip it.
+		except:																											
 			continue
 		
-		if difficulty == 'intermediate':														#Reducing the difficulty levels to three by combining synonyms
+		#Reducing the difficulty levels to three by combining synonyms
+		if difficulty == 'intermediate':														
 			difficulty = 'medium'
 		if difficulty == 'difficult':
 			difficulty = 'hard'
 		
-							
-		if not os.path.exists(difficulty + '/' + challenge):				#Make a directory if it does not already exist
+		#Make a directory if it does not already exist							
+		if not os.path.exists(difficulty + '/' + challenge):				
 			os.makedirs(difficulty + '/' + challenge)					
-		filename = difficulty + '/' + challenge + '/problem.txt'		#Set pathname of file to be <difficulty>/<challengenumber>/problem.txt
+		#Set pathname of file to be <difficulty>/<challengenumber>/problem.txt
+		filename = difficulty + '/' + challenge + '/problem.txt'		
 		if not os.path.exists(filename):
 			f = open(filename, 'w')
 			f.write(('http://www.reddit.com' + permalink + '\n\n').encode('utf-16'))
 			f.write(desc.encode('utf-16'))
 			f.close()
-			counter += 1																							#Increment global counter to keep track of number of files we've written on this run
+			#Increment global counter to keep track of number of files we've written on this run
+			counter += 1																							
 		
 def main():
-	listing = getListing()																				#Get the page listing from reddit using the API
-	after = listing['after']																			#Get last post in listing for pagination
+	#Get the page listing from reddit using the API
+	listing = getListing()																				
+	#Get last post in listing for pagination
+	after = listing['after']																			
 	while (after != None):
-		savePostsToFile(listing['children'])												#Start saving the listing's children (ie, stories) to files
-		listing = getListing(after)																	#Get the next listing after the post marked 'after'
+		#Start saving the listing's children (ie, stories) to files
+		savePostsToFile(listing['children'])				
+		#Get the next listing after the post marked 'after'								
+		listing = getListing(after)																	
 		after = listing['after']
 	print('Done! Wrote ' + str(counter) + ' files.')	
 	
